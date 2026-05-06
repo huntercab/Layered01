374843e21d8670f95bf76fe67ebbd07652a993df
Carlos Bonilla <cab989@gmail.com>
2026-05-05 19:52:08 -0500

    UnitTests were added.

---
+++ CartService.UnitTests/CartItemTests.cs
@@ -0,0 +1,35 @@
+﻿using CartService.Domain.Entities;
+using CartService.Domain.ValueObjects;
+using Xunit;
+
+namespace CartService.UnitTests
+{
+    public class CartItemTests
+    {
+        [Fact]
+        public void Constructor_ShouldCreateCartItem_WhenDataIsValid()
+        {
+            var item = new CartItem(1,"itemName1", Money.Create(25,"USD"),2);
+
+            Assert.Equal(1,item.Id);
+            Assert.Equal("itemName1", item.Name);
+            Assert.Equal(2,item.Quantity);
+        }
+
+        [Fact]
+        public void Constructor_ShouldThrowException_WhenNameIsEmpty()
+        {
+            Assert.Throws<ArgumentException>(() => 
+                new CartItem(1,"", Money.Create(25,"USD"),1)
+            );
+        }
+
+        [Fact]
+        public void Constructor_ShouldThrowException_WhenQuantityIsInvalid()
+        {
+            Assert.Throws<ArgumentException>(() =>
+                new CartItem(1, "ietmName1", Money.Create(25, "USD"), 0)
+            );
+        }
+    }
+}
---
+++ CartService.UnitTests/MoneyTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CartService.Domain.ValueObjects;
+using Xunit;
+
+namespace CartService.UnitTests
+{
+    public class MoneyTests
+    {
+        [Fact]
+        public void Create_ShouldCreateMoney_WhenDataIsValid()
+        {
+            var money = Money.Create(25, "USD");
+
+            Assert.Equal(25, money.Amount);
+            Assert.Equal("USD", money.Currency);
+        }
+
+        [Fact]
+        public void Create_ShouldThrowException_WhenAmountIsNegative()
+        {
+            Assert.Throws<ArgumentException>(() => Money.Create(-1,"USD"));
+        }
+
+        [Fact]
+        public void Create_ShouldThrowException_WhenCurrencyIsInvalid()
+        {
+            Assert.Throws<ArgumentException>(() => Money.Create(25, "US"));
+        }
+    }
+}
